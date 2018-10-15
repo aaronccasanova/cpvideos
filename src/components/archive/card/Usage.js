18c6fd9d7fecfdf76799e5adb97551688359be9b
Aaron Casanova <aaronccasanova@gmail.com>
2018-10-15 16:16:48 -0700

    update to create-react-app 2.0 and start 3d card

--- src/components/archive/card/Usage.js
+++ src/components/archive/card/Usage.js
@@ -12,13 +12,12 @@ class Usage extends Component {
         {/* --- Place Usage Code Below --- */}
         {/* <LeftyLoosy /> */}
         <Card
+          grayscale
           size="300px"
           speed="350ms"
           title="Moose"
           subtitle="Wazanova"
-          body="Will you be heard? Do you have a voice? Do you feel heard right now?"
-          // frontImg="https://images.unsplash.com/photo-1539470138620-515ac36ac716?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5194f3b58ce8e288b7230990f2008fa7&auto=format&fit=crop&w=934&q=80"
-          // backImg="https://images.unsplash.com/photo-1509624776920-0fac24a9dfda?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=42ec196ddd2f51e9dbb7bfe0adcbaa3b&auto=format&fit=crop&w=1950&q=80"
+          src="/moose.jpg"
         />
       </Wrapper>
     );
--- src/components/playground/Source.js
+++ src/components/playground/Source.js
@@ -1,147 +1,138 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Wrapper = styled.div`
-  @import url('https://fonts.googleapis.com/css?family=Archivo+Black');
-  /* ---- CSS Variables Section ----- */
-  --size: ${props => (props.size ? props.size : '200px')};
-  --speed: ${props => (props.speed ? props.speed : '350ms')};
-  --color: #f3f3f3;
-  /* -------------------------------- */
-  font: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+const HoverAreaWrapper = styled.div`
+  /* border: 2px solid red; */
+  --card-width: ${props => (props.cardWidth ? props.cardWidth : '300px')};
+  --card-height: ${props => (props.cardHeight ? props.cardHeight : '300px')};
+  --front-background: ${props =>
+    props.frontBackground ? props.frontBackground : '#DEDEDE'};
+  --back-background: ${props =>
+    props.backBackground ? props.backBackground : '#BABABA'};
+  --front-img: ${props => (props.frontImg ? `url(${props.frontImg})` : '')};
+  --back-img: ${props => (props.backImg ? `url(${props.backImg})` : '')};
+  --front-layer1: ${props =>
+    props.frontLayer1 ? props.frontLayer1 : 'translateZ(35px)'};
+  --front-layer2: ${props =>
+    props.frontLayer2 ? props.frontLayer2 : 'translateZ(95px)'};
+  --front-layer3: ${props =>
+    props.frontLayer3 ? props.frontLayer3 : 'translateZ(120px)'};
+  --outline-padding: ${props =>
+    props.outlinePadding ? props.outlinePadding : '20px'}
+  --outline-border: ${props =>
+    props.outlineBorder ? props.outlineBorder : '2px solid #191919'}
+    --border-radius: ${props =>
+      props.borderRadius ? props.borderRadius : '5px'};
+    --flip-duration: ${props =>
+      props.flipDuration ? props.flipDuraction : '1700ms'};
+    --flip-bezier: ${props =>
+      props.flipBezier
+        ? props.flipBezier
+        : 'cubic-bezier(0.82, 0.51, 0.67, 0.97)'};
+        /* : 'cubic-bezier(0.71, 0.46, 0.49, 0.82)'}; */
+        /* : 'cubic-bezier(0.82, 0.34, 0.51, 0.83)'}; */
+
+  transition: all var(--flip-duration) var(--flip-bezier);
+`;
+
+const Card = styled.div`
+  width: var(--card-width);
+  height: var(--card-height);
+  min-width: var(--card-width);
+  min-height: var(--card-height);
   position: relative;
-  width: var(--size);
-  height: var(--size);
-  background: #222;
 
-  * {
-    color: var(--color);
+  transform-style: preserve-3d;
+  transition: all var(--flip-duration) var(--flip-bezier);
+  ${HoverAreaWrapper}:hover & {
+    transform: rotateY(0.5turn);
   }
 
-  /* inset outline */
+  /*
+   * @outline 
+   */
   &::before {
     content: '';
     position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border: 1px solid var(--color);
-    opacity: 0.8;
-    z-index: 3;
-    width: 85%;
-    height: 85%;
-  }
+    top: var(--outline-padding);
+    bottom: var(--outline-padding);
+    right: var(--outline-padding);
+    left: var(--outline-padding);
+    border-radius: var(--border-radius);
 
-  /* overlay */
-  &::after {
-    content: '';
-    position: absolute;
-    z-index: 2;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: rgba(0, 0, 0, 0.1);
-    opacity: 0;
-    transition: opacity var(--speed);
-  }
-
-  &:hover {
-    &::after {
-      opacity: 1;
-    }
+    border: var(--outline-border);
+    transform: var(--front-layer1);
   }
 `;
 
-const Img = styled.img`
-  filter: ${props => (props.grayscale ? 'grayscale(100%)' : null)};
+const FrontBackStyles = css`
   position: absolute;
   top: 0;
-  left: 0;
-  z-index: 1;
-  width: 100%;
-  height: 100%;
-  color: var(--color);
-  object-fit: cover;
-`;
-
-const TitleWrapper = styled.div`
-  position: absolute;
   bottom: 0;
-  left: 25%;
-  right: 25%;
-  z-index: 4;
+  right: 0;
+  left: 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   text-align: center;
-  transform: translateY(0%);
-  transition: transform var(--speed) cubic-bezier(0.53, 0.2, 0.38, 0.81);
+  padding: 20px;
+  border-radius: var(--border-radius);
+  background-size: cover;
+  background-blend-mode: overlay;
+  box-shadow: 1px 3px 12px rgba(0, 0, 0, 0.15);
 
-  ${Wrapper}:hover & {
-    transition: transform var(--speed) cubic-bezier(0.65, -0.01, 0.54, 0.97);
-    transform: translateY(-50%);
-  }
+  backface-visibility: hidden;
+  transform-style: preserve-3d;
 `;
 
-const Title = styled.div`
-  font-family: 'Archivo Black', sans-serif;
-  text-transform: uppercase;
-  position: relative;
-  width: 100%;
-  font-size: calc(var(--size) / 10);
-  line-height: 1;
-  opacity: 0.8;
-  transition: opacity var(--speed) cubic-bezier(0.52, 0.12, 0.57, 0.67);
+const Front = styled.div`
+  ${FrontBackStyles};
 
-  /* title underline */
-  &::after {
-    content: '';
-    display: block;
-    width: 100%;
-    height: 1px;
-    margin: 7% auto 8% auto;
-    background-color: var(--color);
-    opacity: 0.8;
-    transform: scale(0);
-    transition: transform calc(var(--speed) / 2);
-  }
-
-  ${Wrapper}:hover & {
-    opacity: 1;
-
-    &::after {
-      transform: scale(1);
-      transition: transform calc(var(--speed) / 2) calc(var(--speed) / 2);
-    }
-  }
+  background: var(--front-background);
+  background-image: var(--front-img);
 `;
 
-const Subtitle = styled.div`
-  font-size: calc(var(--size) / 20);
-  line-height: 1;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  opacity: 0;
-  transition: opacity calc(var(--speed) / 2);
+const Back = styled.div`
+  ${FrontBackStyles};
 
-  ${Wrapper}:hover & {
-    opacity: 0.8;
-    transition: opacity calc(var(--speed) / 2) calc(var(--speed) / 2);
-  }
+  background: var(--back-background);
+  background-image: var(--back-img);
+  transform: rotateY(0.5turn);
 `;
 
-const Source = ({ title, subtitle, src, size, speed, grayscale }) => {
+const Title = styled.h2`
+  font-size: 30px;
+  transform: var(--front-layer3);
+`;
+
+const Subtitle = styled.p`
+  font-size: 16px;
+  transform: var(--front-layer2);
+`;
+
+const Body = styled.p`
+  font-weight: bold;
+  color: #595959;
+  transform: var(--front-layer2);
+`;
+
+const Source = ({ title, subtitle, body, src, frontImg, backImg }) => {
   return (
-    <Wrapper size={size} speed={speed}>
-      <Img src={src} alt={title} grayscale={grayscale} />
-      <TitleWrapper>
-        <Title>{title}</Title>
-        <Subtitle>{subtitle}</Subtitle>
-      </TitleWrapper>
-    </Wrapper>
+    <HoverAreaWrapper frontImg={frontImg} backImg={backImg}>
+      <Card>
+        <Front>
+          <Title>{title}</Title>
+          <Subtitle>{subtitle}</Subtitle>
+        </Front>
+        <Back>
+          <Body>{body}</Body>
+        </Back>
+      </Card>
+    </HoverAreaWrapper>
   );
 };
 
 export default Source;
-
-// src="https://images.pexels.com/photos/840326/pexels-photo-840326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-// src="https://www.dropbox.com/s/8cc22nblu4y4vzi/jenny.jpeg?raw=1"
